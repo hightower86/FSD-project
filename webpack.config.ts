d95441d636bcc7e3e550217eadacbca4f9465f16
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2022-09-20 20:19:04 +0300

    dev-server with env vars

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,23 +2,27 @@ import path from 'path'
 
 import webpack from 'webpack'
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildPaths } from './config/build/types/config';
+import { BuildPaths, BuildEnv } from './config/build/types/config';
 
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  build: path.resolve(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html')
+
+
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html')
+  }
+
+  const mode = env.mode || 'development'
+  const isDev = mode === 'development'
+  const PORT = env.port || 3000
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT
+  });
+
+  return config
 }
-
-const mode = 'development'
-const isDev = mode === 'development'
-const PORT = 3000
-
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: 'development',
-  paths,
-  isDev,
-  port: PORT
-});
-
-export default config

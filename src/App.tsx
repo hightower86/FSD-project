77d74f8880debecb5f8e077ee07467d4af9b542e
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2022-09-23 08:50:37 +0300

    theme variables and theme toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import "./styles/index.scss";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+  };
+
   return (
-    <div className="app">
+    <div className={`app ${theme === Theme.LIGHT ? Theme.LIGHT : Theme.DARK}`}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+      <hr />
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Suspense fallback={<div>...Loading</div>}>
         <Routes>
           <Route path={"/about"} element={<AboutPageAsync />} />

0e1f49b7277acfb2f6250e1e63e9ab22decf23b6
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2022-09-24 21:10:01 +0300

    ClasNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
 import { Suspense } from "react";
 import "./styles/index.scss";
 import { useTheme } from "./theme/useThemeContext";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <hr />

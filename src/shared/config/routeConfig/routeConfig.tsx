c75d87e325ceafd993daad66beb1f5d30fe01cb2
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2022-09-26 19:39:38 +0300

    Module-2. AppRouter and Router config

---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,25 @@
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { RouteProps } from "react-router-dom";
+
+export enum AppRoutes {
+  MAIN = "main",
+  ABOUT = "about",
+}
+
+export const routePath: Record<AppRoutes, string> = {
+  [AppRoutes.MAIN]: "/",
+  [AppRoutes.ABOUT]: "./About",
+};
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+  [AppRoutes.MAIN]: {
+    path: routePath.main,
+    element: <MainPage />,
+  },
+
+  [AppRoutes.ABOUT]: {
+    path: routePath.about,
+    element: <AboutPage />,
+  },
+};

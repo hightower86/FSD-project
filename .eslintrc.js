170c859a1ea8b2c53f1a69d1d4b6178d82f8e7c6
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2022-10-07 16:08:02 +0300

    Module-2. les-19. i18n eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['plugin:react/recommended', 'airbnb'],
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
@@ -34,6 +38,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         '@typescript-eslint/no-unused-vars': ['warn'],
         'react/jsx-wrap-multilines': ['warn', {}],
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
         __IS_DEV__: true,

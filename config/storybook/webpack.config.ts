e2c3d6491dfb1c05c47b1e9f00b28429ef57f37c
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2022-10-19 18:29:00 +0300

    M3-l3 story Sidebar

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import buildCssLoader from '../../config/build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
@@ -13,6 +13,18 @@ export default ({ config }: { config: webpack.Configuration }) => {
     config.resolve?.modules?.push(paths.src);
     config.resolve?.extensions?.push('.ts', '.tsx');
 
+    // eslint-disable-next-line no-param-reassign
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
+        return rule;
+    });
+
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
     config.module?.rules?.push(buildCssLoader(true));
 
     return config;

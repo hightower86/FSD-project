eef350995dc94e81305a34c780dda267e75e8159
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-04-10 09:44:41 +0400

    M5-L40-2 react/types version syncronized. profile translations added

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -20,12 +20,12 @@ export default ({ config }: { config: webpack.Configuration }) => {
         return rule;
     });
 
-    config.module?.rules.push({
+    config.module!.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config.module?.rules?.push(buildCssLoader(true));
-    config.plugins?.push(
+    config.module!.rules.push(buildCssLoader(true));
+    config.plugins!.push(
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(true),
             __API__: JSON.stringify(''),

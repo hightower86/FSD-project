7ffdb4c0a16686a1e92cf5542810ddd2ef5696a0
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-04-17 19:24:19 +0400

    new components styles fixes

--- src/entities/Article/model/services/fetchArticleById/fetchArticleById.ts
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.ts
@@ -6,22 +6,19 @@ export const fetchArticleById = createAsyncThunk<
     Article,
     string,
     ThunkConfig<string>
-    >(
-        'articleDetails/fetchArticleById',
-        async (articleId, thunkApi) => {
-            const { extra, rejectWithValue } = thunkApi;
+>('articleDetails/fetchArticleById', async (articleId, thunkApi) => {
+    const { extra, rejectWithValue } = thunkApi;
 
-            try {
-                const response = await extra.api.get<Article>(`/articles/${articleId}`);
+    try {
+        const response = await extra.api.get<Article>(`/articles/${articleId}`);
 
-                if (!response.data) {
-                    throw new Error();
-                }
+        if (!response.data) {
+            throw new Error();
+        }
 
-                return response.data;
-            } catch (e) {
-                console.log(e);
-                return rejectWithValue('error');
-            }
-        },
-    );
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        return rejectWithValue('error');
+    }
+});
--- src/shared/ui/Text/Text.stories.tsx
+++ src/shared/ui/Text/Text.stories.tsx
@@ -2,7 +2,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Text, TextTheme } from './Text';
+import { Text, TextSize, TextTheme } from './Text';
 
 export default {
     title: 'shared/Text',
@@ -55,3 +55,10 @@ OnlyTextDark.args = {
     text: 'This is a Title.',
 };
 OnlyTextDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const SizeL = Template.bind({});
+SizeL.args = {
+    title: 'Title lorem ipsun',
+    text: 'Description Description Description Description',
+    size: TextSize.L,
+};

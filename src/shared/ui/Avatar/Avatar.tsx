70d14913bac31d8c871c607a76e2dc66a87d0e49
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-04-18 16:51:39 +0400

    add comments list

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -9,16 +9,16 @@ interface AvatarProps {
     alt?: string;
 }
 
-export const Avatar = ({
-    className, src, size, alt,
-}: AvatarProps) => {
+export const Avatar = ({ className, src, size, alt }: AvatarProps) => {
     const mods: Mods = {};
 
-    const styles = useMemo<CSSProperties>(() => ({
-        width: size || 100,
-        height: size || 100,
-    }), [size]);
-
+    const styles = useMemo<CSSProperties>(
+        () => ({
+            width: size || 100,
+            height: size || 100,
+        }),
+        [size]
+    );
     return (
         <img
             src={src}

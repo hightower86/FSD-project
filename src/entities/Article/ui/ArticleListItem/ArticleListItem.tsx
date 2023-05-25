fd8efeb8e13c2b8720ac071711778fac837ba64b
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-05-25 18:37:43 +0400

    Small type article view and new Card UI component

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,5 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { Text } from 'shared/ui/Text/Text';
+import { Icon } from 'shared/ui/Icon/Icon';
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import { Card } from 'shared/ui/Card/Card';
 import cls from './ArticleListItem.module.scss';
 import { Article, ArticleView } from '../../model/types/article';
 
@@ -9,13 +14,46 @@ interface ArticleListItemProps {
     view: ArticleView;
 }
 
-export const ArticleListItem = (props: ArticleListItemProps) => {
+export const ArticleListItem = memo((props: ArticleListItemProps) => {
     const { className, article, view } = props;
     const { t } = useTranslation('articles');
 
+    if (view === ArticleView.BIG) {
+        return (
+            <div
+                className={classNames(cls.ArticleListItem, {}, [
+                    className,
+                    cls[view],
+                ])}
+            >
+                {article.title}
+            </div>
+        );
+    }
+
     return (
-        <div className={classNames(cls.ArticleListItem, {}, [className])}>
-            {article.title}
+        <div
+            className={classNames(cls.ArticleListItem, {}, [
+                className,
+                cls[view],
+            ])}
+        >
+            <Card className={cls.card}>
+                <div className={cls.imageWrapper}>
+                    <img
+                        alt={article.title}
+                        src={article.img}
+                        className={cls.img}
+                    />
+                    <Text text={article.createdAt} className={cls.date} />
+                </div>
+                <div className={cls.infoWrapper}>
+                    <Text text={article.type.join(', ')} className={cls.type} />
+                    <Text text={String(article.views)} className={cls.views} />
+                    <Icon Svg={EyeIcon} />
+                </div>
+                <Text text={article.title} className={cls.title} />
+            </Card>
         </div>
     );
-};
+});

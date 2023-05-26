623fa7622426d2fa37e2beda221230cd927a43e4
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-05-26 08:32:50 +0400

    ArticleListSkeleton

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,6 +67,7 @@ module.exports = {
         'no-param-reassign': 'off',
         'operator-linebreak': 'off',
         'no-undef': 'off',
+        'react/no-array-index-key': 'warn',
     },
     globals: {
         __IS_DEV__: true,
